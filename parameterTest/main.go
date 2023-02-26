c2de764514a464dc27b2db73fe884b9b5bfb23bb
Yasuyuki Kikuchi <yasuyuki0321@gmail.com>
2023-02-27 00:41:22 +0900

    feat: 全パラメータを取得するように変更

--- parameterTest/main.go
+++ parameterTest/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
-	"net/http/httputil"
 	"net/url"
 	"os"
 	"time"
 
 	runtime "github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/aws/aws-sdk-go/aws"
 )
 
 type resultFromExtension struct {
@@ -29,18 +32,55 @@ type resultFromExtension struct {
 	} `json:"ResultMetadata"`
 }
 
+type parameter struct {
+	Name  string `json:"Name"`
+	Value string `json:"Value"`
+}
+
 type response struct {
-	Parameter  resultFromExtension `json:"parameter"`
-	StatusCode int                 `json:"statusCode"`
+	Parameters []parameter `json:"parameters"`
+	StatusCode int         `json:"statusCode"`
 }
 
 func handleRequest() (response, error) {
-	param, _ := getParameterValue("secure-string-test")
+	paramNames, err := getAllParameters()
+	if err != nil {
+		return response{}, err
+	}
+
+	var parameters = make([]parameter, 0)
+	for _, paramName := range paramNames {
+		param, _ := getParameterValue(paramName)
+
+		parameters = append(parameters, parameter{
+			Name:  param.Parameter.Name,
+			Value: param.Parameter.Value,
+		})
+	}
 
 	return response{
-		Parameter:  param,
+		Parameters: parameters,
 		StatusCode: http.StatusOK,
 	}, nil
+}
+
+func getAllParameters() ([]string, error) {
+	cfg, _ := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
+	svc := ssm.NewFromConfig(cfg)
+
+	out, err := svc.GetParametersByPath(context.TODO(), &ssm.GetParametersByPathInput{
+		Path: aws.String("/"),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	paramNames := make([]string, 0)
+	for i := range out.Parameters {
+		paramNames = append(paramNames, *out.Parameters[i].Name)
+	}
+
+	return paramNames, nil
 }
 
 func getParameterValue(parameterPath string) (resultFromExtension, error) {
@@ -50,8 +90,8 @@ func getParameterValue(parameterPath string) (resultFromExtension, error) {
 	req, _ := http.NewRequest("GET", requestURL, nil)
 	req.Header.Set("X-Aws-Parameters-Secrets-Token", os.Getenv("AWS_SESSION_TOKEN"))
 
-	dump, _ := httputil.DumpRequestOut(req, true)
-	fmt.Printf("%s", dump)
+	// dump, _ := httputil.DumpRequestOut(req, true)
+	// fmt.Printf("%s", dump)
 
 	client := new(http.Client)
 	resp, err := client.Do(req)
@@ -60,8 +100,8 @@ func getParameterValue(parameterPath string) (resultFromExtension, error) {
 	}
 	defer resp.Body.Close()
 
-	dumpResp, _ := httputil.DumpResponse(resp, true)
-	fmt.Printf("%s", dumpResp)
+	// dumpResp, _ := httputil.DumpResponse(resp, true)
+	// fmt.Printf("%s", dumpResp)
 
 	var resultFromExtension resultFromExtension
 	body, _ := io.ReadAll(resp.Body)
